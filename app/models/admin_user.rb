0c33dee50b158334f1f747d31fe98b41bae69cbd
Jeremy J Hartmann <jhartman@sfu.ca>
2016-06-20 18:56:13 -0700

    Fix class name change in model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,8 @@
 class AdminUser < ActiveRecord::Base
+	#When change name of class, make sure to change name of file to admin_user
 	# To configure a differnt table name
 	# self.table_name = "admin_user"
 	
+	has_and_belongs_to_many :pages
 
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ActiveRecord::Base
 	 # Relations
 	 belongs_to :subject
+	 has_and_belongs_to_many :editors, :class_name => "AdminUser"
 	# has_many :sections
 end

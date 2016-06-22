0674ef367246e2d5f9fc48afe28438b5fae290bf
Jeremy J Hartmann <jhartman@sfu.ca>
2016-06-21 20:38:48 -0700

    Add CRUD functinality to Section and Page
    Create erb files
    Create controller logic.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,14 +14,54 @@ class PagesController < ApplicationController
   def create
     # Instantiate object
     # Assign values
-    # Redirect
+    @page = Page.new(page_params)
+
+    if @page.save
+      # Redirect
+      flash[:notice] = "The #{@page.name} page has been added."
+      redirect_to({:action => 'index'})
+    else
+      # Render the new page again
+      render('new')
+    end
+
   end
 
   def edit
     @page = Page.find(params[:id])
   end
 
+  def update
+    @page = Page.find(params[:id])
+
+    # Update page and save to db
+    if @page.update_attributes(page_params)
+      flash[:notice] = "The @{page.name} page has updated successfully."
+
+      # Redirct to the show page......
+      redirect_to(:action => 'show', :id => @page.id)
+    else
+      render('edit')
+    end
+
+  end
+
   def delete
     @page = Page.find(params[:id])
   end
+  
+  def destroy
+    @page = Page.find(params[:id]).destroy
+
+    flash[:notice] = "Page '#{@page.name}' has been deleted."
+
+    redirect_to(:action => 'index')
+  end
+
+  # Private Methods/Vars
+  private 
+  def page_params 
+    params.require(:page).permit(:name, :permalink, :position, :visible)
+  end
+  
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,16 +1,63 @@
 class SectionsController < ApplicationController
   def index
+    @sections = Section.all
   end
 
   def show
+    @section = Section.find(params[:id])
   end
 
   def new
+    @section = Section.new({:name => 'Default', :content_type => "Blog"})
+  end
+
+  def create
+    # Instantiate the section
+    @section = Section.new(section_params)
+
+    # Check it saved
+    if @section.save
+
+      flash[:notice] = "Section '#{@section.name}' Saved."
+      redirect_to(:action => 'index')
+
+    else
+      render('new')
+    end
   end
 
   def edit
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:id])
+
+    if @section.update_attributes(section_params)
+
+      flash[:notice] = "Update '#{@section.name}' successfully"
+      redirect_to(:action => 'show', :id => @section.id)
+    else
+      render('edit')
+    end
+
   end
 
   def delete
+    @section = Section.find(params[:id])
+  end
+
+  def destroy
+
+    @section = Section.find(params[:id]).destroy
+
+    flash[:notice] = "The section '#{@section.name}' has been deleted."
+
+    redirect_to(:action => 'index')
+  end
+
+  private
+  def section_params
+    params.require(:section).permit(:name, :position, :visible, :content_type, :content)
   end
 end
